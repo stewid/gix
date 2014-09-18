611241c8e2d67ea3421db2c24f38d4b070d104ea
Stefan Widgren <stefan.widgren@gmail.com>
2014-09-18 07:43:16 +0200

    Add 'R/-gix-package.r'

---
+++ R/gix-package.r
@@ -0,0 +1,27 @@
+## gix, search a Git repository using the Xapian search engine.
+## Copyright (C) 2014 Stefan Widgren
+##
+##  gix is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  gix is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License along
+##  with this program; if not, write to the Free Software Foundation, Inc.,
+##  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+##' Search a Git repository
+##'
+##' gix is a package to index and search a Git repository using the
+##' Xapian search engine.
+##' @docType package
+##' @name gix
+##' @import git2r
+##' @import xapr
+##' @include gix.r
+NULL
